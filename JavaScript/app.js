cb8ed97f067d700d5a93c4d13ae68b8a62eb0a90
Benjamin-D-Ramos <144626890+Benjamin-D-Ramos@users.noreply.github.com>
2023-10-12 19:58:07 -0300

    Update app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,4 +1,37 @@
-alert("Hola, puedo decirte cuantos °C equivalen en °F")
-let Celsius = parseInt(prompt("Ingresa la temperatura en °C"))
-alert(`Calculando...`)
-alert(`${Celsius} °C equivalen a ${(Celsius*5/9) + 32} °F`)
+// Reorganización de Elementos​
+// Tienes un array de números: [10, 20, 30, 40, 50]. Mueve el último número al principio del array
+
+let arr = [10, 20, 30, 40, 50]
+
+arr.pop();
+arr.push(10);
+
+arr.shift()
+arr.unshift(50)
+
+console.log(arr);
+
+
+// Extracción y Unión de Arrays
+// Tienes dos arrays: numeros1 con elementos [1, 2, 3] y numeros2 con elementos [4, 5, 6].
+// Utiliza los métodos pop y push para mover el último elemento de numeros1 al final de numeros2, y luego muestra el array numeros2.
+
+let arr1 = [1, 2, 3]
+let arr2 = [4, 5, 6]
+
+arr1.pop();
+arr1.push();
+
+arr2.push(3);
+
+console.log(arr1);
+console.log(arr2);
+
+
+// Inversión de Array​
+// Dado un array de números: [5, 10, 15, 20, 25], crea un nuevo array que contenga los mismos elementos pero en orden inverso
+
+let arr3 = [5, 10, 15, 20, 25]
+arr3.reverse()
+
+console.log(arr3);
