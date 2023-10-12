8e535e3b4ac60c40eb2cd21bb46b85173e632950
Benjamin-D-Ramos <144626890+Benjamin-D-Ramos@users.noreply.github.com>
2023-10-12 12:45:17 -0300

    Add files via upload

---
+++ JavaScript/app.js
@@ -0,0 +1,4 @@
+alert("Hola, puedo decirte cuantos °C equivalen en °F")
+let Celsius = parseInt(prompt("Ingresa la temperatura en °C"))
+alert(`Calculando...`)
+alert(`${Celsius} °C equivalen a ${(Celsius*1.8) + 32} °F`)
