e248df0578c623783ea67612085cc68a51418402
Benjamin-D-Ramos <144626890+Benjamin-D-Ramos@users.noreply.github.com>
2023-10-20 17:23:59 -0300

    Update app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,34 +1,103 @@
-// Reorganización de Elementos​
-// Tienes un array de números: [10, 20, 30, 40, 50]. Mueve el último número al principio del array
+// Ejercicio 1: Escribe un programa que solicite al usuario su edad y muestre un mensaje indicando si es mayor de edad o no.​
 
-let arr = [10, 20, 30, 40, 50]
+// let edad = parseInt(prompt("ingrese su edad"))
 
-arr.pop();
-arr.unshift(50)
-
-console.log(arr);
-
-
-// Extracción y Unión de Arrays
-// Tienes dos arrays: numeros1 con elementos [1, 2, 3] y numeros2 con elementos [4, 5, 6].
-// Utiliza los métodos pop y push para mover el último elemento de numeros1 al final de numeros2, y luego muestra el array numeros2.
-
-let arr1 = [1, 2, 3]
-let arr2 = [4, 5, 6]
-
-arr1.pop();
-arr1.push();
-
-arr2.push(3);
-
-console.log(arr1);
-console.log(arr2);
+// switch (true) {
+//     case edad >= 18:
+//         alert("Eres mayor de edad")
+//         break;
+//     case edad <= 17:
+//         alert("eres menor de edad")
+//         break;
+// }
 
 
-// Inversión de Array​
-// Dado un array de números: [5, 10, 15, 20, 25], crea un nuevo array que contenga los mismos elementos pero en orden inverso
+// Ejercicio 2: Escribe un programa que solicite al usuario dos números y muestre cuál de ellos es mayor.​
 
-let arr3 = [5, 10, 15, 20, 25]
-arr3.reverse()
+// let num1 = parseInt(prompt("ingrese el primer numero"))
+// let num2 = parseInt(prompt("ingrese el segundo numero"))
 
-console.log(arr3);
+// switch (true) {
+//     case num1 > num2:
+//         alert("el numero mayor es: " + num1)
+//         break;
+//     case num2 > num1:
+//         alert("el numero mayor es: " + num2)
+// }
+
+
+// Ejercicio 3: Escribe un programa que solicite al usuario un número y muestre un mensaje indicando si es positivo, negativo o cero.
+
+// let numero = parseInt(prompt("ingrese numero"))
+
+// switch (true) {
+//     case numero > 0:
+//         alert("el numero es positivo")
+//         break;
+//     case 0 > numero:
+//         alert("el numero es negativo")
+//         break;
+//     case numero == 0:
+//         alert("el numero es neutro")
+// }
+
+
+// Ejercicio 4: Escribe un programa que solicite al usuario un número del 1 al 7 y muestre el día de la semana correspondiente.
+// Si el número no está en ese rango, mostrar un mensaje de error.
+
+// let numero = parseInt(prompt("ingrese numero"))
+
+// switch (true) {
+//     case numero == 1:
+//         alert("Hoy es lunes")
+//         break;
+//     case numero == 2:
+//         alert("Hoy es martes")
+//         break;
+//     case numero == 3:
+//         alert("Hoy es miércoles")
+//         break;
+//     case numero == 4:
+//         alert("Hoy es jueves")
+//         break;
+//     case numero == 5:
+//         alert("Hoy es viernes")
+//         break;
+//     case numero == 6:
+//         alert("Hoy es sábado")
+//         break;
+//     case numero == 7:
+//         alert("Hoy es domingo")
+//         break;
+// }
+
+// if (numero < 1 || numero > 7){
+//     alert("error")
+// }
+
+
+// Ejercicio 5: Escribe un programa que solicite al usuario su calificación en un examen y muestre un mensaje indicando si ha aprobado
+// (calificación mayor o igual a 60) o ha reprobado (calificación menor a 60).
+
+
+// let calificación = parseInt(prompt("Ingrese su calificación"));
+
+// if (calificación <= 60) {
+//     alert("Desaprobado")
+// }
+// if (calificación >= 59) {
+//     alert("Aprobado")
+// }
+
+
+// Ejercicio 6: Escribe un programa que solicite al usuario dos números y a uno le sume 10 y al otro le reste 2.
+
+// let numero1 = parseInt(prompt("ingrese primer numero"))
+// let numero2 = parseInt(prompt("ingrese segundo numero"))
+
+// if (numero1){
+//     alert("el primer numero le sumamos 10 y da un resultado de "+ (numero1+10))
+// }
+// if (numero2){
+//     alert("el segundo numero lo restamos 2 y dio este resultado " + (numero2-2))
+// }
