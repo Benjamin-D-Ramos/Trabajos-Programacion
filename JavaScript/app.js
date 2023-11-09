a2e62a93d2765f4e8afe46cc627ed102e4e06066
Benjamin-D-Ramos <144626890+Benjamin-D-Ramos@users.noreply.github.com>
2023-11-09 14:51:48 -0300

    Update app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,103 +1,30 @@
-// Ejercicio 1: Escribe un programa que solicite al usuario su edad y muestre un mensaje indicando si es mayor de edad o no.​
+// Ejercicio de Bucle For: Imprimir los números del 1 al 10 en la consola.
 
-// let edad = parseInt(prompt("ingrese su edad"))
-
-// switch (true) {
-//     case edad >= 18:
-//         alert("Eres mayor de edad")
-//         break;
-//     case edad <= 17:
-//         alert("eres menor de edad")
-//         break;
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
 // }
 
+// Ejercicio de Bucle While: Imprimir los números pares del 2 al 20 en la consola.​
 
-// Ejercicio 2: Escribe un programa que solicite al usuario dos números y muestre cuál de ellos es mayor.​
+// let count = 0;
 
-// let num1 = parseInt(prompt("ingrese el primer numero"))
-// let num2 = parseInt(prompt("ingrese el segundo numero"))
-
-// switch (true) {
-//     case num1 > num2:
-//         alert("el numero mayor es: " + num1)
-//         break;
-//     case num2 > num1:
-//         alert("el numero mayor es: " + num2)
+// while (count < 20) {
+//     count += 2;
+//     console.log(count);
 // }
 
+// Ejercicio de Bucle Do-While: Pedir al usuario que ingrese un número y
+// seguir solicitando un nuevo número hasta que ingrese un número negativo.
+// Imprimir la suma de los números ingresados. O detener el bucle y que muestre un mensaje diciendo muchas gracias!
 
-// Ejercicio 3: Escribe un programa que solicite al usuario un número y muestre un mensaje indicando si es positivo, negativo o cero.
+let i = 0;
+let num;
 
-// let numero = parseInt(prompt("ingrese numero"))
+do {
+    num = parseInt(prompt("Ingrese un número:"));
+    if (num >= 0) {
+        i += num;
+    }
+} while (num >= 0);
 
-// switch (true) {
-//     case numero > 0:
-//         alert("el numero es positivo")
-//         break;
-//     case 0 > numero:
-//         alert("el numero es negativo")
-//         break;
-//     case numero == 0:
-//         alert("el numero es neutro")
-// }
-
-
-// Ejercicio 4: Escribe un programa que solicite al usuario un número del 1 al 7 y muestre el día de la semana correspondiente.
-// Si el número no está en ese rango, mostrar un mensaje de error.
-
-// let numero = parseInt(prompt("ingrese numero"))
-
-// switch (true) {
-//     case numero == 1:
-//         alert("Hoy es lunes")
-//         break;
-//     case numero == 2:
-//         alert("Hoy es martes")
-//         break;
-//     case numero == 3:
-//         alert("Hoy es miércoles")
-//         break;
-//     case numero == 4:
-//         alert("Hoy es jueves")
-//         break;
-//     case numero == 5:
-//         alert("Hoy es viernes")
-//         break;
-//     case numero == 6:
-//         alert("Hoy es sábado")
-//         break;
-//     case numero == 7:
-//         alert("Hoy es domingo")
-//         break;
-// }
-
-// if (numero < 1 || numero > 7){
-//     alert("error")
-// }
-
-
-// Ejercicio 5: Escribe un programa que solicite al usuario su calificación en un examen y muestre un mensaje indicando si ha aprobado
-// (calificación mayor o igual a 60) o ha reprobado (calificación menor a 60).
-
-
-// let calificación = parseInt(prompt("Ingrese su calificación"));
-
-// if (calificación <= 60) {
-//     alert("Desaprobado")
-// }
-// if (calificación >= 59) {
-//     alert("Aprobado")
-// }
-
-
-// Ejercicio 6: Escribe un programa que solicite al usuario dos números y a uno le sume 10 y al otro le reste 2.
-
-// let numero1 = parseInt(prompt("ingrese primer numero"))
-// let numero2 = parseInt(prompt("ingrese segundo numero"))
-
-// if (numero1){
-//     alert("el primer numero le sumamos 10 y da un resultado de "+ (numero1+10))
-// }
-// if (numero2){
-//     alert("el segundo numero lo restamos 2 y dio este resultado " + (numero2-2))
-// }
+alert("La suma de los números ingresados es: " + i);
