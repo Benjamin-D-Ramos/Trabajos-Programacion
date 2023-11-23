bce9331d89434b20f1a1a8798728bfd1267b8ad9
Benjamin-D-Ramos <144626890+Benjamin-D-Ramos@users.noreply.github.com>
2023-11-23 14:43:04 -0300

    Update app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,81 +1,15 @@
-// Convertidor de Temperatura:
-// Escribe funciones para convertir entre diferentes escalas de temperatura, como Celsius, Fahrenheit y Kelvin.​
+  // Escribe una función que divide un arreglo (primer argumento)
+  // en grupos de la longitud size(segundo argumento) y los devuelva 
+  // como un arreglo bidimensional.
 
-//NOTA: fuente de las formulas https://www.how-to-study.com/metodos-de-estudio/escalas-de-temperatura.asp
-
-function calculadordeTemperatura() {
-
-    let temperatura1 = prompt("Ingrese tipo de temperatura (Celsius, Fahrenheit o Kelvin)").toLocaleLowerCase().charAt(0)
-    let num1 = parseFloat(prompt("Ingrese la temperatura"))
-    let temperatura2 = prompt("Ingrese temperatura que quiera saber el resultado (Celsius, Fahrenheit o Kelvin)").toLocaleLowerCase().charAt(0)
-
-    switch (temperatura1) {
-        case "c":
-            switch (temperatura2) {
-                case "f":
-                    alert(`El resultado de ${num1}° Celsius es: ${num1 * 1.8 + 32}° Fahrenheit`);
-                    break;
-                case "k":
-                    alert(`El resultado de ${num1}° Celsius es: ${num1 + 273.15}° Kelvin`);
-                    break;
-            }
-            break;
-
-        case "f":
-            switch (temperatura2) {
-                case "c":
-                    alert(`El resultado de ${num1}° Fahrenheit es: ${(num1 - 32) / 1.8}° Celsius`);
-                    break;
-                case "k":
-                    alert(`El resultado de ${num1}° Fahrenheit es: ${(5 / 9) * (num1 - 32) + 273.15}° Kelvin`);
-                    break;
-            }
-            break;
-
-        case "k":
-            switch (temperatura2) {
-                case "c":
-                    alert(`El resultado de ${num1}° Kelvin es: ${num1 - 273.15}° Celsius`);
-                    break;
-                case "f":
-                    alert(`El resultado de ${num1}° Kelvin es: ${(1.8 * (num1 - 273.15)) + 32}° Fahrenheit`);
-                    break;
-            }
-            break;
+  function chunkArrayInGroups(array, size,) {
+    let resultado = [];
+    for (let i = 0; i < array.length; i += size) {
+      resultado.push(array.slice(i, i + size));
     }
-}
+    return resultado;
+  }
+  
+  chunkArrayInGroups(["a", "b", "c", "d"], 2);
 
-calculadordeTemperatura();
-
-
-// Suma de Números en un Rango:
-// Diseña una función que calcule la suma de todos los números en un rango dado.
-
-alert(`Sumare todos los números en un rango`)
-
-function sumaRango() {
-    let num1 = parseFloat(prompt("Ingrese el primer número"))
-    let num2 = parseFloat(prompt("Ingrese el segundo número"))
-    let suma = 0
-
-    for (let i = num1; i <= num2; i++) {
-        suma += i
-    }
-
-    alert(`La suma de los números en el rango es: ${suma}`)
-}
-
-sumaRango();
-
-
-// Contador de Palabras:
-// Escribe una función que cuente la cantidad de palabras en una cadena de texto.
-
-alert(`Te dire cuantas palabras tiene una frase`)
-
-function contadorPalabras() {
-    let frase = prompt("Ingrese una frase")
-    let palabras = frase.split(" ").length
-    alert(`La frase tiene ${palabras} palabras`)
-}
-contadorPalabras();
+  console.log(chunkArrayInGroups(["1", "2", "3", "4", "5", "6", "7", "8",], 2));
